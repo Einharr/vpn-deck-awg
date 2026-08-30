4ef302b066c144eb5049594e6988314c517002de
Einharr <atamanplatov@gmail.com>
2026-08-30 22:02:48 +0300

    fix: use verified Steam Deck service manager

--- py_modules/vpn_deck/__init__.py
+++ py_modules/vpn_deck/__init__.py
@@ -4,7 +4,7 @@ from .binary_manager import BinaryManager
 from .config_manager_ext import ConfigManager
 from .diagnostics import Diagnostics
 from .protocol import analyse_config, detect_protocol
-from .service_manager import ServiceManager
+from .service_manager_ext import ServiceManager
 from .settings_manager import SettingsManager
 
 __all__ = [
